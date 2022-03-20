bea1649dc148cddbcb823943268cb8328243d258
asdi80 <a8sadi@gmail.com>
2022-03-20 00:19:25 -0700

    deploy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(PORT, () => console.log("Server Running"));
-app.use(express.static(path.resolve(__dirname, "build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 
 const contactEmail = nodemailer.createTransport({
