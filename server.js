93915de727c09b5674a11914ad77000def1e7d60
asdi80 <a8sadi@gmail.com>
2022-03-19 22:10:11 -0700

    deploy

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 require('dotenv').config();
 const path = require("path");
+const PORT = process.env.PORT || 5000;
+
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(PORT, () => console.log("Server Running"));
 app.use(express.static(path.resolve(__dirname, "build")));
 
 
