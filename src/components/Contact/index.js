1a14db81e7e1b2267549ac005b0d6c8e5725351d
asdi80 <a8sadi@gmail.com>
2022-03-20 22:26:32 -0700

    deploy

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -16,7 +16,7 @@ const Contact = () => {
       email: email.value,
       message: message.value,
     };
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("https://react-profilee.herokuapp.com/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
